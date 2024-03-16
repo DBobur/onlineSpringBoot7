d0d827a51ad9f877ebe29d0b119d3c260ae3983c
bobur <davlatboyev2024@gmail.com>
2024-03-16 00:12:31 -0700

    Initial commit

---
+++ src/main/java/com/example/onlinespringboot7/OnlineSpringBoot7Application.java
@@ -0,0 +1,13 @@
+package com.example.onlinespringboot7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineSpringBoot7Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnlineSpringBoot7Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/onlinespringboot7/OnlineSpringBoot7ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.onlinespringboot7;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineSpringBoot7ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
